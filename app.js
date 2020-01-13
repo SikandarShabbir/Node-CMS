31badba2221083097a76dfce883c86a24c51a1dc
Sikki-Dev <sikandarshabbir11@gmail.com>
2020-01-12 18:40:26 -0800

    "Work in progress"

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/cms').then(db=>{
+	console.log("Mongo Connected");
+}).catch(error=>console.log(error));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,8 +16,12 @@ app.set('view engine', 'handlebars');
 
 const home = require('./routes/home/index');
 app.use('/', home);
+
 const admin = require('./routes/admin/index');
 app.use('/admin', admin);
+
+const posts = require('./routes/admin/posts');
+app.use('/admin', posts);
 
 
 
