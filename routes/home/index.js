fecdf9c70649e7a39730fa86e2696b209ede438f
Sikki-Dev <37258226+Sikki-Dev@users.noreply.github.com>
2020-01-20 00:39:13 +0500

    user registration.

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post.js');
 const Category = require('../../models/Category.js');
+const User = require('../../models/User.js');
+const bcrypt = require('bcryptjs');
 router.all('/*', (request, response, next)=>{
 	request.app.locals.layout = 'home';
 	next();
@@ -23,6 +25,51 @@ router.get('/login', (request, response)=>{
 });
 router.get('/register', (request, response)=>{
 	response.render('home/register');
+});
+router.post('/register', (request, response)=>{
+	let errors = [];
+	if (!request.body.firstName) {
+		errors.push({message: "Please enter firstName"});
+	}
+	if (!request.body.lastName) {
+		errors.push({message: "Please enter lastName"});
+	}
+	if (!request.body.email) {
+		errors.push({message: "Please enter email"});
+	}
+	if (!request.body.password) {
+		errors.push({message: "Please enter password"});
+	}
+	if (request.body.password != request.body.passwordConfirm) {
+		errors.push({message: "Passwords dont match"});
+	}
+	if (errors.length > 0) {
+		response.render('home/register',{ errors: errors });
+	}else{
+		const createUser = new User({
+		firstName: request.body.firstName,
+		lastName: request.body.lastName,
+		email: request.body.email,
+		password: request.body.password,
+		}) 
+		bcrypt.genSalt(10, (err, salt)=>{
+			bcrypt.hash(createUser.password, salt, (err, hash)=>{
+				createUser.password = hash;
+				createUser.save()
+				.then( createUser => {
+					request.flash('success', 'User Created Successfully please login.');
+					// console.log("Success: "+createUser);
+					response.render('home/login');
+				})
+				.catch(error =>{
+					console.log("Error: "+error);
+				});
+			})
+		})
+		
+	}
+
+	
 });
 router.get('/post/:id', (request, response)=>{
 	Post.find({_id: request.params.id})
